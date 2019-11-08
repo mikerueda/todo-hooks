1ccc55ee0bd64aa4c2bbd1f0f6579293e1847f70
marcreativo <marcreativo@gmail.com>
2019-11-08 18:56:42 -0300

    Updates

---
+++ precache-manifest.7cb569bf358c4b2d13fca4a50ae99f43.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b16c714be4684c7b795c1c513e8a538",
+    "url": "/todo-hooks/index.html"
+  },
+  {
+    "revision": "1ca0a9f7b4bd67183781",
+    "url": "/todo-hooks/static/css/main.ea6fe5e1.chunk.css"
+  },
+  {
+    "revision": "c1fd8bd22b3b2f5ea952",
+    "url": "/todo-hooks/static/js/2.b41efde7.chunk.js"
+  },
+  {
+    "revision": "1ca0a9f7b4bd67183781",
+    "url": "/todo-hooks/static/js/main.393f45fd.chunk.js"
+  },
+  {
+    "revision": "23de50f2c7142ed0a71c",
+    "url": "/todo-hooks/static/js/runtime-main.e4d8e411.js"
+  }
+]);
